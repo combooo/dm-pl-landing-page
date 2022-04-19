1dd8f2d08e1b5fe77ab9625f2a85af3269e95e34
Stanislav Kvitko <sokvitko@gmail.com>
2022-04-19 12:40:02 +0300

    burger menu

--- scripts.js
+++ scripts.js
@@ -1,6 +1,9 @@
 (function() {
     const burger = document.querySelector('.burger');
+    const menu = document.querySelector('.menu__list-mobile');
+    
     burger.addEventListener('click', () => {
         burger.classList.toggle('burger__active');
+        menu.classList.toggle('menu__list-mobile-active');
     })
 }());
