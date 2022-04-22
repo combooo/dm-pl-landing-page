f32aa8319485c8b4aa72539257e084864051a7a3
Stanislav Kvitko <sokvitko@gmail.com>
2022-04-22 19:20:51 +0300

    test form sending

---
+++ mail.php
@@ -0,0 +1,49 @@
+<?php
+    use PHPMailer\PHPMailer\PHPMailer;
+    use PHPMailer\PHPMailer\Exception;
+
+    require 'PHPMailer-6.6.0/src/Exception.php';
+    require 'PHPMailer-6.6.0/src/PHPMailer.php';
+
+    $mail = new PHPMailer(true);
+    $mail->CharSet = 'UTF-8';
+    $mail->setLanguage('pl', 'phpmailer/language/');
+    $mail->IsHTML(true);
+
+    //from
+    $mail->setFrom('info@fls.guru', 'NameOfSender');
+    //receiver
+    $mail->addAddress('sokvitko@gmail.com');
+    //topic
+    $mail->Subject = 'Hey, this is a test';
+
+    //Hand
+    $hand = "Right";
+    if($_POST['hand'] == "left") {
+        $hand = "Left";
+    }
+
+    //Letter body
+    $body = '<h1>This is super test letter!</h1>';
+
+    if(trim(!empty($_POST['name']))){
+        $body.='<p><strong>Name:</strong> '.$_POST['name'].'</p>';
+    }
+    if(trim(!empty($_POST['email']))){
+        $body.='<p><strong>E-mail:</strong> '.$_POST['mail'].'</p>';
+    }
+
+    $mail->Body = $body;
+
+    //Sending
+    if (!$mail->send()) {
+        $message = 'error';
+    } else {
+        $message = 'Data sent';
+    }
+
+    $response = ['message' => $message];
+
+    header('Content-type: application/json');
+    echo json_encode($response);
+?>
--- scripts.js
+++ scripts.js
@@ -1,3 +1,5 @@
+'use strict'
+
 (function() {
     const burger = document.querySelector('.burger');
     const menu = document.querySelector('.menu__list-mobile');
@@ -7,3 +9,64 @@
         menu.classList.toggle('menu__list-mobile-active');
     })
 }());
+
+document.addEventListener('DOMContentLoaded', function() {
+    const form = document.getElementById('form-header');
+    form.addEventListener('submit', sendForm);
+
+    async function sendForm(e) {
+        e.preventDefault();
+
+        let formData = new FormData(form);
+
+        let response = await fetch('mail.php', {
+            method: 'POST',
+            body: formData
+        });
+        if (response.ok) {
+            let result = await response.json();
+            alert(result.message);
+            formPreview.innerHTML = '';
+            form.reset();
+        } else {
+
+        }
+        //let error = formValidate();
+    }
+
+    function formValidate(form) {
+        let error = 0;
+        let formReq = document.querySelectorAll('._req');
+
+        for (let index = 0; index < formReq.length; index++) {
+            const input = formReq[index];
+            removeFormError(input);
+
+            if(input.classList.contains('_email')) {
+                if(emailTest(input)) {
+                    formAddError(input);
+                    error++;
+                }
+            } else {
+                if (input.value === '') {
+                    formAddError(input);
+                    error++;
+                }
+            }
+        }
+    }
+
+    function addFormError(input) {
+        input.parentElement.classList.add('_error');
+        input.classList.add('_error');
+    }
+
+    function removeFormError(input) {
+        input.parentElement.classList.remove('_error');
+        input.classList.remove('_error');
+    }
+
+    function emailTest(input) {
+        return !/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,8})+$/.test(input.value);
+    }
+});
