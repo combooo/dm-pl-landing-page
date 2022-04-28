c64b74c5f8de6fbe1d791c2dc58a99b599f20d42
Stanislav Kvitko <sokvitko@gmail.com>
2022-04-28 19:37:16 +0300

    add modal window and fix input styles

--- scripts.js
+++ scripts.js
@@ -8,63 +8,15 @@
     })
 }());
 
-document.addEventListener('DOMContentLoaded', function() {
-    const form = document.getElementById('form-header');
-    form.addEventListener('submit', sendForm);
-
-    async function sendForm(e) {
-        e.preventDefault();
-
-        let formData = new FormData(form);
-
-        let response = await fetch('mail.php', {
-            method: 'POST',
-            body: formData
-        });
-        if (response.ok) {
-            let result = await response.json();
-            alert(result.message);
-            formPreview.innerHTML = '';
-            form.reset();
-        } else {
-
-        }
-        //let error = formValidate();
-    }
-
-    function formValidate(form) {
-        let error = 0;
-        let formReq = document.querySelectorAll('._req');
-
-        for (let index = 0; index < formReq.length; index++) {
-            const input = formReq[index];
-            removeFormError(input);
-
-            if(input.classList.contains('_email')) {
-                if(emailTest(input)) {
-                    formAddError(input);
-                    error++;
-                }
-            } else {
-                if (input.value === '') {
-                    formAddError(input);
-                    error++;
-                }
-            }
-        }
-    }
-
-    function addFormError(input) {
-        input.parentElement.classList.add('_error');
-        input.classList.add('_error');
-    }
-
-    function removeFormError(input) {
-        input.parentElement.classList.remove('_error');
-        input.classList.remove('_error');
-    }
-
-    function emailTest(input) {
-        return !/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,8})+$/.test(input.value);
-    }
-});
+/* document.addEventListener("DOMContentLoaded", function(){
+    var scrollbar = document.body.clientWidth - window.innerWidth + 'px';
+    console.log(scrollbar);
+    document.querySelector('[href="#openModal"]').addEventListener('click',function(){
+        document.body.style.overflow = 'hidden';
+        document.querySelector('#openModal').style.marginLeft = scrollbar;
+    });
+    document.querySelector('[href="#close"]').addEventListener('click',function(){
+        document.body.style.overflow = 'visible';
+        document.querySelector('#openModal').style.marginLeft = '0px';
+    });
+}); */
