0eb8d551e3764a28002a81f16216c697fd3a56cf
Stanislav Kvitko <sokvitko@gmail.com>
2022-04-29 21:18:10 +0300

    fix css styles

--- scripts.js
+++ scripts.js
@@ -1,22 +1,9 @@
-(function() {
-    const burger = document.querySelector('.burger');
-    const menu = document.querySelector('.menu__list-mobile');
-    
-    burger.addEventListener('click', () => {
-        burger.classList.toggle('burger__active');
-        menu.classList.toggle('menu__list-mobile-active');
-    })
-}());
+(function () {
+  const burger = document.querySelector('.burger');
+  const menu = document.querySelector('.menu__list-mobile');
 
-/* document.addEventListener("DOMContentLoaded", function(){
-    var scrollbar = document.body.clientWidth - window.innerWidth + 'px';
-    console.log(scrollbar);
-    document.querySelector('[href="#openModal"]').addEventListener('click',function(){
-        document.body.style.overflow = 'hidden';
-        document.querySelector('#openModal').style.marginLeft = scrollbar;
-    });
-    document.querySelector('[href="#close"]').addEventListener('click',function(){
-        document.body.style.overflow = 'visible';
-        document.querySelector('#openModal').style.marginLeft = '0px';
-    });
-}); */
+  burger.addEventListener('click', () => {
+    burger.classList.toggle('burger__active');
+    menu.classList.toggle('menu__list-mobile-active');
+  });
+}());
